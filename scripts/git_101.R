98b7ac45759ef5ed39678a4fd2bc659e7dc037bd
Belson Malcolm Kutambe <bdkutambe@gmail.com>
2022-12-12 15:20:35 +0200

    not okay change

--- scripts/git_101.R
+++ scripts/git_101.R
@@ -1,5 +1,5 @@
 library(ggplot2)
 
 mlw_meta_df <- read.csv('/Users/malcolmorian/Documents/Bioinformatics/Projects2022/HappyAssignment/combined-profiles.Malawi.tsv')
-#THIS IS OKAY
+#THIS IS not OKAY
 
